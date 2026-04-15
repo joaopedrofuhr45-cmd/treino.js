e9e2fbf73445cd56a3d3b40a64fa704ea941f9c6
joaopedrofuhr45-cmd <joaopedrofuhr45@gmail.com>
2026-04-14 22:45:44 -0300

    .

--- financeFlowModular/src/shared/middlewares/errorHandler.js
+++ financeFlowModular/src/shared/middlewares/errorHandler.js
@@ -1,18 +1,32 @@
-export function validarNovoUsuario(req, res, next) {
-    const { email, password, name } = req.body
+import { AppError } from "../errors/AppError.js"
 
-    if (!email || !password || !name) {
-        return res.status(400).json({ message: "precisa inseriri os dados para constinuar" })
-    }
-
-    if (!email.includes("@")) {
-        return res.status(400).json({ message: "o email precisa de @" })
-    }
-
-    if (password.length < 6) {
-        return res.status(400).json({
-            message: "a senha precisa de mais de 6 caracteres"
+export function erroHandler(err, req, res, next) {
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message
         })
     }
-    next()
+
+    if (err instanceof SyntaxError) {
+        return res.status(400).json({
+            message: "json invalido"
+        })
+    }
+
+    // Passo 3: Erro de JWT?
+    if (err.name === "JsonWebTokenError") {
+        return res.status(401).json({ message: "token inválido" })
+    }
+
+    if (err.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "token expirado" })
+    }
+
+    // Passo 4: Erro genérico
+    return res.status(500).json({ message: "erro interno" })
+
+    next(err)
 }
+
+
+
